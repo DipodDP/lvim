92e3b9d8777b6a297e9020bf587262f5f46cb257
DipodDP <dipoddp@gmail.com>
2024-06-03 17:00:33 +0000

    Resore builtins after update

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -376,13 +376,13 @@ M.config = function()
   lvim.builtin.treesitter.context_commentstring.enable = true
   local languages = vim
     .iter({
-      { "bash", "c", "c_sharp", "cmake", "comment", "cpp", "css", "d", "dart" },
-      { "dockerfile", "elixir", "elm", "erlang", "fennel", "fish", "go", "gomod" },
-      { "gomod", "graphql", "hcl", "vimdoc", "html", "java", "javascript", "jsdoc" },
-      { "json", "jsonc", "julia", "kotlin", "latex", "ledger", "lua", "make" },
-      { "markdown", "markdown_inline", "nix", "ocaml", "perl", "php", "python" },
-      { "query", "r", "regex", "rego", "ruby", "rust", "scala", "scss", "solidity" },
-      { "swift", "teal", "toml", "tsx", "typescript", "vim", "vue", "yaml", "zig" },
+      -- { "bash", "c", "c_sharp", "cmake", "comment", "cpp", "css", "d", "dart" },
+      -- { "dockerfile", "elixir", "elm", "erlang", "fennel", "fish", "go", "gomod" },
+      -- { "gomod", "graphql", "hcl", "vimdoc", "html", "java", "javascript", "jsdoc" },
+      -- { "json", "jsonc", "julia", "kotlin", "latex", "ledger", "lua", "make" },
+      -- { "markdown", "markdown_inline", "nix", "ocaml", "perl", "php", "python" },
+      -- { "query", "r", "regex", "rego", "ruby", "rust", "scala", "scss", "solidity" },
+      -- { "swift", "teal", "toml", "tsx", "typescript", "vim", "vue", "yaml", "zig" },
     })
     :flatten()
     :totable()
